96fce26752bfe933ac958a17a20c58e96f6a3d36
ihthisham <ummerihthisham@gmail.com>
2015-05-01 18:37:03 +0530

    Vowel_Check

---
+++ Java Assignments/src/Vowel.java
@@ -0,0 +1,21 @@
+
+public class Vowel {
+
+	public static void main(String[] args) {
+		
+		String alpha="U";
+		
+		if(alpha == "a"|| alpha=="A" || alpha=="e" || alpha=="E" || alpha=="i"
+				|| alpha=="I" || alpha=="o" || alpha=="O" || alpha=="u" || alpha=="U")
+		{
+			System.out.println(" " +alpha+ " is Vowel");
+		}
+		
+		else 
+		{
+			System.out.println( " " + alpha + " is Consonant");
+		}
+		
+	}
+
+}
