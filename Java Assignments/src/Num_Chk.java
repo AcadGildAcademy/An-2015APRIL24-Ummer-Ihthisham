f89d859bfbba48a4dc694b5d62a61b7cb9b0ee39
ihthisham <ummerihthisham@gmail.com>
2015-05-01 16:15:34 +0530

    Number Check (Even or Odd)

---
+++ Java Assignments/src/Num_Chk.java
@@ -0,0 +1,18 @@
+
+public class Num_Chk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number=4;
+		
+		System.out.println("Entered Number:"+number);
+		
+		if(number%2==0)
+		{
+			System.out.println("The Number Is Even");
+		}
+		else
+			System.out.println("The Number is Odd");
+}
+}
